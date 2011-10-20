5d6ce230dac92a33fd0c57725545b55b8ec84259
Thierry Poullin <thierrypoullin@gmail.com>
2011-10-20 22:46:10 +0200

    Ajout de split sur les mots + recherche sur plusieurs mots

---
+++ dotSearch.Site/dotSearchResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace dotSearch.Site
+{
+    public class dotSearchResult
+    {
+        public string pageTitle;
+        public string pageUrl;
+        public string pageDescription;
+        public int dotPriority;
+        public Guid pageID;
+    }
+
+    public class dotHelper
+    {
+        public static List<string> splitQuery(string query)
+        {
+            List<string> wordsList = new List<string>();
+            
+            string pattern = @"(-""[^""]+""|""[^""]+""|-\w+|\w+)\s*"; 
+
+            MatchCollection mc = Regex.Matches(query, pattern);
+            foreach (Match m in mc)
+            {
+                wordsList.Add(m.Groups[0].Value.Trim());
+            }
+
+            return wordsList;
+        }
+    }
+}
--- dotSearch.Site/searchResults.aspx.cs
+++ dotSearch.Site/searchResults.aspx.cs
@@ -8,6 +8,7 @@ using dotSearchDataContext;
 using System.Data;
 using System.Data.Linq;
 using System.Diagnostics;
+using dotSearch.Site;
 
 public partial class searchResults : System.Web.UI.Page
 {
@@ -27,74 +28,139 @@ public partial class searchResults : System.Web.UI.Page
                 var index = new Lucene.Linq.DatabaseIndexSet<dotBaseDataContext>(
                     @"C:\Index",
                     new dotBaseDataContext());
-                index.Write();
+                index.Write();                
 
-                List<dotSearchDataContext.Page> pageList = new List<dotSearchDataContext.Page>();
-                long begin = DateTime.Now.Ticks;
-                List<dotSearchDataContext.Word> queryWord = (from w in index.DataContext.Words
-                                                             where w.txt_word.Contains(userQuery) || w.txt_word.Equals(userQuery)
-                                                             select w).ToList();
+                #region Split de la requete de l'utilisateur
+                List<string> queryList = dotHelper.splitQuery(userQuery);
+                #endregion
 
-                foreach (dotSearchDataContext.Word item in queryWord)
+
+                if (queryList.Count > 0)
                 {
-                    List<dotSearchDataContext.Occurrence> occurences = (from o in index.DataContext.Occurrences
-                                                                        where o.id_word.Equals(item.id_word)
-                                                                        select o).ToList();
-
-                    foreach (dotSearchDataContext.Occurrence occur in occurences)
+                    #region Recherche d'elements
+                    int priorite = 0;
+                    long begin = DateTime.Now.Ticks;
+                    //On recupere toutes les pages contenant le premier mot de la query
+                    List<dotSearchResult> resultList = (from p in index.DataContext.Pages
+                                                        join o in index.DataContext.Occurrences
+                                                            on p.id_page equals o.id_page
+                                                        join w in index.DataContext.Words
+                                                            on o.id_word equals w.id_word
+                                                        where w.txt_word.Contains(queryList[0]) || w.txt_word.Equals(queryList[0])
+                                                        orderby o.nb_occur ascending
+                                                        select new dotSearchResult
+                                                        {
+                                                            pageTitle = p.title_page,
+                                                            pageUrl = p.url_page,
+                                                            pageDescription = p.description_page,
+                                                            pageID = p.id_page
+                                                        }).ToList();
+                    //On affecte la priorite a chaque page
+                    resultList = resultList.AsEnumerable().Select(p => new dotSearchResult()
                     {
-                        dotSearchDataContext.Page page = (from p in index.DataContext.Pages
-                                                          where p.id_page.Equals(occur.id_page)
-                                                          select p).FirstOrDefault();
-                        if (!pageList.Contains(page))
-                            pageList.Add(page);
+                        pageTitle = p.pageTitle,
+                        pageUrl = p.pageUrl,
+                        pageDescription = p.pageDescription,
+                        pageID = p.pageID,
+                        dotPriority = priorite++
+                    }).ToList();
+
+                    if (queryList.Count > 1)
+                    {
+                        //Pour les pages trouvees dans resultList on cherche les pages contenant les autres mots de la query 
+                        for (int j = 1; j < queryList.Count; j++)
+                        {
+                            List<dotSearchResult> tempList = (from p in resultList
+                                                        join o in index.DataContext.Occurrences
+                                                            on p.pageID equals o.id_page
+                                                        join w in index.DataContext.Words
+                                                            on o.id_word equals w.id_word
+                                                        where w.txt_word.Contains(queryList[j]) || w.txt_word.Equals(queryList[j])
+                                                        orderby o.nb_occur ascending
+                                                        select p).ToList();
+
+                            if (tempList != null)
+                            {
+                                //On affecte la priorite a chaque page
+                                tempList = tempList.AsEnumerable().Select(p => new dotSearchResult()
+                                                    {
+                                                        pageTitle = p.pageTitle,
+                                                        pageUrl = p.pageUrl,
+                                                        pageDescription = p.pageDescription,
+                                                        pageID = p.pageID,
+                                                        dotPriority = priorite++
+                                                    }).ToList();
+
+                                //On ajoute les nouveaux resultats aux resultats initiaux
+                                resultList.AddRange(tempList); 
+                            }
+                        }
                     }
+
+                    //Calcul du temps d'execution de requete
+                    long end = DateTime.Now.Ticks;
+                    long totalTime = end - begin;
+                    TimeSpan ts = TimeSpan.FromTicks(totalTime);
+                    double seconds = ts.TotalSeconds;
+
+                    //On etablit la liste finale de resultats sans doublons et triee
+                    List<dotSearchResult> finalResult = new List<dotSearchResult>();
+                    foreach (dotSearchResult pageResult in resultList)
+                    {
+                        if (!finalResult.Contains(pageResult))
+                        {
+                            try
+                            {
+                                dotSearchResult match = (from p in resultList
+                                                         where p.pageID.Equals(pageResult.pageID)
+                                                         orderby p.dotPriority descending
+                                                         select p).First();
+
+                                finalResult.Add(match);
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+                        }
+                    }
+
+                    finalResult = finalResult.OrderByDescending(p => p.dotPriority).ToList();
+                    #endregion                    
+
+                    #region Binding a la ListView
+                    DataTable dt = new DataTable();
+
+                    dt.Columns.Add();
+
+                    DataTable dtt = new DataTable();
+                    dtt.Columns.Add("Title");
+                    dtt.Columns.Add("URL");
+                    dtt.Columns.Add("Description");
+
+
+                    if (finalResult.Count == 0)
+                        resultsNbr.Text = "Aucun résultat trouvé";
+                    else
+                        resultsNbr.Text = finalResult.Count + " résultat(s) trouvé(s) en " + Math.Round(seconds, 3) + "s";
+
+                    foreach (dotSearchResult item in finalResult)
+                    {
+                        string url = item.pageUrl;
+                        string title = item.pageTitle;
+                        string description = item.pageDescription;
+
+                        string[] infoArray = { title, url, description };
+
+                        dtt.LoadDataRow(infoArray, LoadOption.OverwriteChanges);
+                    }
+                    resultsList.DataSource = dtt;
+                    resultsList.DataBind();
+
+                    dtt.Dispose();
+                    firstTime = false; 
+                    #endregion
                 }
-
-                List<dotSearchDataContext.Page> pageList2 = (from p in pageList
-                                                             join oc in index.DataContext.Occurrences
-                                                             on p.id_page equals oc.id_page
-                                                             orderby oc.nb_occur ascending
-                                                             select p).ToList();
-
-                pageList2.Reverse();
-
-
-                long end = DateTime.Now.Ticks;
-                long totalTime = end - begin;
-                TimeSpan ts = TimeSpan.FromTicks(totalTime);
-                double seconds = ts.TotalSeconds;
-
-                DataTable dt = new DataTable();
-
-                dt.Columns.Add();
-
-                DataTable dtt = new DataTable();
-                dtt.Columns.Add("Title");
-                dtt.Columns.Add("URL");
-                dtt.Columns.Add("Description");
-
-
-                if (pageList.Count == 0)
-                    resultsNbr.Text = "Aucun résultat trouvé";
-                else
-                    resultsNbr.Text = pageList.Count + " résultat(s) trouvé(s) en " + Math.Round(seconds, 3) + "s";
-
-                foreach (dotSearchDataContext.Page item in pageList2)
-                {
-                    string url = item.url_page;
-                    string title = item.title_page;
-                    string description = item.description_page;
-
-                    string[] infoArray = { title, url, description };
-
-                    dtt.LoadDataRow(infoArray, LoadOption.OverwriteChanges);
-                }
-                resultsList.DataSource = dtt;
-                resultsList.DataBind();
-
-                dtt.Dispose();
-                firstTime = false;
             }
         }
     }
